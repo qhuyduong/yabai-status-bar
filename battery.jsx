1171d528280d71fcec841877f8d5d6e796102732
Huy Duong <huy.duong@employmenthero.com>
2019-06-24 00:24:35 +0700

    Update battery
    Signed-off-by: Huy Duong <huy.duong@employmenthero.com>

--- battery.jsx
+++ battery.jsx
@@ -1,16 +1,46 @@
+import get from 'lodash/fp/get';
 import { battery } from './style.jsx';
 
-export const command =
-  "pmset -g batt | egrep '([0-9]+%).*' -o --colour=auto | cut -f1 -d'%'";
+export const command = 'sh yabai-status-bar/scripts/battery.sh';
 
-export const refreshFrequency = 5000; // ms
+export const refreshFrequency = 2000; // ms
+
+const parse = input => {
+  try {
+    return JSON.parse(input);
+  } catch (e) {
+    return undefined;
+  }
+};
 
 export const className = battery;
 
-export const render = ({ output }) => (
-  <div>
-    <i className="fas fa-battery-full" style={{ color: 'red' }} />
-    &nbsp;
-    {output}
-  </div>
-);
+const iconPicker = (level, status) => {
+  if (status === 'AC') {
+    return 'fa-bolt';
+  } else if (level < 20) {
+    return 'fa-battery-empty';
+  } else if (level < 40) {
+    return 'fa-le-quarter';
+  } else if (level < 60) {
+    return 'fa-battery-half';
+  } else if (level < 80) {
+    return 'fa-battery-three-quarters';
+  } else {
+    return 'fa-battery-full';
+  }
+};
+
+export const render = ({ output }) => {
+  let data = parse(output);
+
+  return (
+    <div>
+      <i
+        className={`fas ${iconPicker(get('level')(data), get('status')(data))}`}
+      />
+      &nbsp;
+      {get('level')(data)}
+    </div>
+  );
+};
--- scripts/battery.sh
+++ scripts/battery.sh
@@ -3,4 +3,13 @@
 yabai=/usr/local/bin/yabai
 jq=/usr/local/bin/jq
 
-pmset -g batt | egrep '([0-9]+\%).*' -o --colour=auto | cut -f1 -d'%'
+LEVEL=$(pmset -g batt | egrep '([0-9]+\%).*' -o --colour=auto | cut -f1 -d'%')
+STATUS=$(pmset -g batt | grep "'.*'" | sed "s/'//g" | awk '{print $4}')
+
+echo $(cat <<EOF
+{
+  "level": "$LEVEL",
+  "status": "$STATUS"
+}
+EOF
+)
