145918e7117e28abe1ba203c2335e01d0362372c
Huy Duong <huy.duong@employmenthero.com>
2019-06-26 14:27:31 +0700

    Use red wifi icon if wifi's SSID empty
    Signed-off-by: Huy Duong <huy.duong@employmenthero.com>

--- components/wifi.jsx
+++ components/wifi.jsx
@@ -2,7 +2,10 @@ import get from 'lodash/fp/get';
 
 const Wifi = ({ data }) => (
   <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
-    <i className="fas fa-wifi" style={{ marginRight: 5 }} />
+    <i
+      className="fas fa-wifi"
+      style={{ marginRight: 5, color: !get('ssid')(data).trim() ? 'red' : '' }}
+    />
     <div>{get('ssid')(data)}</div>
   </div>
 );
--- scripts/right_bar.sh
+++ scripts/right_bar.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 FREE_MEMORY=$(top -l 1 | head -n 7 | grep PhysMem | egrep -o '[0-9]+' | tail -1)
-WIFI_SSID=$(networksetup -getairportnetwork en0 | cut -c 24-)
+WIFI_SSID=$(networksetup -getairportnetwork en0 | awk -F": " '{print $2}')
 BATTERY_LEVEL=$(pmset -g batt | egrep '([0-9]+\%).*' -o --colour=auto | cut -f1 -d'%')
 BATTERY_STATUS=$(pmset -g batt | grep "'.*'" | sed "s/'//g" | awk '{print $4}')
 VOLUME_LEVEL=$(osascript -e 'output volume of (get volume settings)')
